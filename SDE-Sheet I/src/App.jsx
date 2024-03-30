3fc3551c8a5dcf59af0203a6388127dd2871ebb2
s1lent-dev <pareshdeshpande2178+github@gmail.com>
2024-03-30 20:04:49 +0530

    updated

--- SDE-Sheet I/src/App.jsx
+++ SDE-Sheet I/src/App.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import './App.scss'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { useState } from 'react'
-import Home from './pages/Home/Home'
-import Navbar from './Components/Navbar/Navbar'
-import Footer from "./Components/Footer/Footer"
-import DsaSheet from './pages/DsaSheet/DsaSheet'
+import React from "react";
+import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import Home from "./pages/Home/Home";
+import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
+import DsaSheet from "./pages/DsaSheet/DsaSheet";
 const App = () => {
   const [islogin, setisLogin] = useState(false);
   return (
     <BrowserRouter>
-    <Navbar setisLogin={setisLogin}/>
-     <Routes>
-       <Route path="/Dsaz/" element={<Home islogin={islogin}/>} />
-       <Route path="/Dsaz/:id" element={<DsaSheet />} />
-     </Routes>
-     <Footer />
-   </BrowserRouter>
-  )
-}
+      <Navbar setisLogin={setisLogin} />
+      <Routes>
+        <Route path="/Dsaz/" element={<Home islogin={islogin} />} />
+        <Route path="/Dsaz/:id" element={<DsaSheet />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
