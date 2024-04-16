a88955df1391aec4442d4f28f44e6cc936c0848c
s1lent-dev <pareshdeshpande2178+github@gmail.com>
2024-04-16 18:18:58 +0530

    updated

--- client/src/Components/Header/Sliderdata.js
+++ client/src/Components/Header/Sliderdata.js
@@ -2,25 +2,25 @@ export const Dsadata = [
     {
         id: 1,
         title: "DSA Sheet I",
-        URL: "/Dsaz/Images/Lc-1.png",
+        URL: "/Images/Lc-1.png",
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, quidem!", 
     },
     {
         id: 2,
         title: "SDE Sheet I",
-        URL: "/Dsaz/Images/Lc-2.png",
+        URL: "/Images/Lc-2.png",
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, quidem!", 
     },
     {
         id: 3,
         title: "SDE Sheet II",
-        URL: "/Dsaz/Images/Lc-3.png",
+        URL: "/Images/Lc-3.png",
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, quidem!", 
     },
     {
         id: 4,
         title: "CP Sheet",
-        URL: "/Dsaz/Images/Lc-4.png",
+        URL: "/Images/Lc-4.png",
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, quidem!", 
     },
 ];
